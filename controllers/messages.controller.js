a5e28807488d567ddd3beb4fe104816efdd5a4e8
natBizitza <romankevich.law@gmail.com>
2022-03-27 11:49:28 +0200

    feat: send files using express

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,12 @@
+const path = require('path');
+
 //changing it here from arrow function to name function to get later more info from node about
 //that function in case of an error
 function getMessages(req, res) {
-  res.send('<ul><li>Hellolu Micky!</li></lu>');
+  //__dirname points to the controller folder
+  //Also here Express sets our content type based on the extesion of our file name
+  res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
+  /* res.send('<ul><li>Hellolu Micky!</li></lu>'); */
 }
 
 function postMessages(req, res) {
